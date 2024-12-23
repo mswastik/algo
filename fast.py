7186998c6d1ead309d74c1bbfa835c015bc98c63
Swastik <m.swastik@gmail.com>
2024-12-23 11:11:44 +0530

    display strategy results

--- fast.py
+++ fast.py
@@ -7,48 +7,77 @@ from fastapi.templating import Jinja2Templates
 import os
 from datetime import datetime
 from typing import Annotated
+import shutil
+from importlib.machinery import SourceFileLoader
+import polars as pl
+import pandas as pd
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-foo = {}
+df=pl.read_parquet(f'C:\\Users\\{os.getlogin()}\\OneDrive\\Python\\stockdata.parquet')
 app.state.it=''
+app.state.df=''
 
 @app.get("/")
-def read_root(request:Request):
-    #return {"Hello": "World"}
+async def read_root(request:Request):
     seq=os.listdir('strategies')
-    return templates.TemplateResponse(request=request,name="index.html",context={"seq":seq})
+    file_list=[f for f in seq if os.path.isfile(os.path.join('strategies',f))]
+    with open(f'symbols.txt') as f:
+        fl=f.readlines()
+    return templates.TemplateResponse(request=request,name="index.html",context={"seq":file_list,"fl":fl})
 
 @app.post("/strategies/{item}")
-def read_item(item: str):
+async def read_item(item: str):
     with open(f'strategies\\{item}') as f:
         fl=f.read()
     app.state.it=item
     print(item)
-    #co=[f"<pre class='strategy'><code class='language-python'>{f}</code></pre>" for f in fl]
-    #print(' '.join(co))
-    #return HTMLResponse(f"<pre class='strategy'><code class='language-python'>{fl}</code></pre>")
-    return HTMLResponse(f'''<div hx-post="/edit" hx-swap="outerHTML" class="mockup-code strategy"><pre><code class="language-python">{fl}</code></pre></div>''')
+    return HTMLResponse(f'''<div hx-post="/edit" hx-swap="outerHTML" class="mockup-code strategy w-full">
+    <button class="btn btn-sm btn-secondary mx-5" hx-post="/run" hx-target="#stats" hx-swap="innerHTML">Run</button>
+    <pre><code class="language-python">{fl}</code></pre></div>''')
 
 @app.post("/edit")
 async def edit_item():
     with open(f'strategies\\{app.state.it}') as f:
         fl=f.read()
-    #co=[f"<pre class='strategy'><code class='language-python'>{f}</code></pre>" for f in fl]
-    #return HTMLResponse(f"<pre class='strategy'><code class='language-python'>{fl}</code></pre>")
-    return HTMLResponse(f'''<div class="mockup-code strategy"><form hx-post="/save/" hx-target=".strategy" hx-swap="outerHTML"><button class="btn btn-primary px-3 mx-5 my-2" type="submit">Save</button>
-        <textarea name="cont" spellcheck="false" class="textarea bg-gray-800 resize-none" 
-        style="white-space: pre; min-width:90%; min-height:600px;">{fl}</textarea></form></div>''')
+    return HTMLResponse(f'''<div class="mockup-code strategy flex"><form hx-post="/save/" hx-target=".strategy" hx-swap="outerHTML" class="space-y-4 p-6 w-full">
+        <button class="btn btn-sm btn-secondary" hx-post="/run" hx-target="#stats" hx-swap="innerHTML">Run</button>
+        <button class="btn btn-sm btn-primary" type="submit">Save</button>
+        <button class="btn btn-sm btn-primary" hx-post="/strategies/{app.state.it}">Cancel</button>
+        <textarea name="cont" spellcheck="false" class="textarea bg-gray-800 w-full" 
+        style="white-space: pre; min-height:600px;">{fl}</textarea></form></div>''')
 
 @app.post("/save/")
 async def save_item(cont: str=Form(...)):
-    #print(cont)
-    os.popen(f'copy {app.state.it} /backup/{app.state.it.join(datetime.now().strftime('%d-%m-%y %M%S'))}')  # Works only in WINDOWS
-    #with open(f'strategies\\backup\\{app.state.it}','w') as f:
-    #    f.write(item.join(datetime.now().strftime()))
-    #co=[f"<pre class='strategy'><code class='language-python'>{f}</code></pre>" for f in fl]
-    #print(' '.join(co))
-    #return HTMLResponse(f"<pre class='strategy'><code class='language-python'>{fl}</code></pre>")
+    shutil.copy(f"strategies/{app.state.it}", f"strategies/backups/{app.state.it}.{datetime.now().strftime('%d-%m-%y_%M%S')}")  # Works only in WINDOWS
+    with open(f'strategies\\{app.state.it}','w') as f:
+        f.write(cont)
     return HTMLResponse(f'<div hx-post="/edit" hx-swap="outerHTML" class="mockup-code strategy"><pre><code class="language-python">{cont}</code></pre></div>')
+
+@app.post("/run")
+async def edit_item():
+    print(app.state.it[:-3])
+    my_module = SourceFileLoader('my_module', f"strategies/{app.state.it}").load_module()
+    bt, stats=my_module.main(app.state.df)
+    st=pd.DataFrame(stats,columns=['stat'])
+    st=st.drop(['_equity_curve','_trades']).to_html()
+    bt.plot(plot_return=False)
+    return st.replace("\n", "")
+
+@app.post("/new")
+async def new_item(filename: str=Form(...)):
+    file_path = f"{filename}.py"
+    if os.path.exists(file_path):
+        return {"message": "File already exists"}
+    with open(f'strategies\\{file_path}', "w") as file:
+        file.write("")
+    return HTMLResponse(f'<div hx-post="/edit" hx-swap="outerHTML" class="mockup-code strategy"><pre><code class="language-python">{cont}</code></pre></div>')
+
+@app.post("/loaddata")
+async def edit_item(symbol: str=Form(...)):
+    df1=df.filter(pl.col('symbol')==symbol)
+    app.state.df=df1
+    #print("loading  ".join(app.state.df))
+    return symbol
--- strategies/macd.py
+++ strategies/macd.py
@@ -3,20 +3,31 @@ from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
 import numpy as np
 import pandas as pd
+import polars as pl
 #dd=pd.DataFrame()
 
 def ta_MACD(df):
-    ""
     return tl.MACD(df,fastperiod=10,slowperiod=20,signalperiod=6)
+
+def ta_OBV(close,vol):
+    return tl.OBV(close,vol)
+
+def ta_stoch(high,low,close):
+    return tl.STOCH(high,low,close)
 
 class MACD(Strategy):
     def init(self):
-        price = self.data['Close']
-        self.macd, self.sig, self.hist =self.I(ta_MACD, df1['Close'])
+        high = np.array([float(x) for x in self.data['High']])
+        low = np.array([float(x) for x in self.data['Low']])
+        close = np.array([float(x) for x in self.data['Close']])
+        vol = np.array([float(x) for x in self.data['Volume']])
+        self.macd, self.sig, self.hist =self.I(ta_MACD, close)
         #self.sma2 = tl.SMA(df['Close'],timeperiod=60)
-        a.dd=pd.DataFrame({'macd':self.macd,'sig':self.sig,'hist':self.hist})
+        self.obv = self.I(ta_OBV,close,vol)
+        self.stoch = self.I(ta_stoch,high,low,close)
+        #a.dd=pd.DataFrame({'macd':self.macd,'sig':self.sig,'hist':self.hist})
     def next(self):
-        if crossover(self.macd, self.sig):
+        if crossover(self.macd, self.sig) and self.stoch<30:
             self.buy()
         elif crossover(self.sig, self.macd):
             try:
@@ -27,10 +38,14 @@ class MACD(Strategy):
                     self.position.close()
             except:
                 pass
-df1=fd.filter(pl.col('symbol')==sym.value).filter(pl.col('epoch')>=dt.date(2021,9,9)).drop('symbol').sort('epoch',descending=False)
-df1=df1.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
-df1=df1.to_pandas()
-df1.set_index('epoch',inplace=True)
-bt = Backtest(df1, MACD,cash= 100000,exclusive_orders=True)
-stats = bt.run()
-bt.plot(plot_return=False,)
+def main(df):
+    desc=""
+    #df1=fd.filter(pl.col('symbol')==sym.value).filter(pl.col('epoch')>=dt.date(2021,9,9)).drop('symbol').sort('epoch',descending=False)
+    df=df.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    df=df.to_pandas()
+    df=df.sort_values('epoch',ascending=True)
+    df.set_index('epoch',inplace=True)
+    bt = Backtest(df, MACD, cash= 100000,exclusive_orders=True)
+    stats = bt.run()
+    #bt.plot(plot_return=False,open_browser=False)
+    return bt,stats
