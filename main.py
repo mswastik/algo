f517db92e7123a96cd2bb3442bcec5bafd5daeb7
Swastik <m.swastik@gmail.com>
2024-12-30 10:25:56 +0530

    added pyecharts

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from importlib.machinery import SourceFileLoader
 import polars as pl
 import pandas as pd
 from backtest import Backtester
+from pyecharts.charts import Bar, Candlestick, Kline, Scatter
+from pyecharts import options as opts
+
+pd.options.display.float_format = '{:,.2f}'.format
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -21,6 +25,14 @@ df=pl.read_parquet(f'C:\\Users\\{os.getlogin()}\\OneDrive\\Python\\stockdata.par
 app.state.it=''
 app.state.df=''
 app.state.fig=''
+app.state.stats=''
+app.state.sym=''
+app.state.tradeplot=False
+app.state.trades=''
+
+col=['Start','End','Duration','Exposure Time [%]','Equity Final [$]','Equity Peak [$]','Return [%]','Buy & Hold Return [%]','Return (Ann.) [%]','Volatility (Ann.) [%]',
+    'Sharpe Ratio',	'Sortino Ratio','Calmar Ratio',	'Max. Drawdown [%]','Avg. Drawdown [%]','Max. Drawdown Duration','Avg. Drawdown Duration','# Trades','Win Rate [%]',
+    'Best Trade [%]','Worst Trade [%]','Avg. Trade [%]','Max. Trade Duration','Avg. Trade Duration','Profit Factor','Expectancy [%]','SQN']
 
 @app.get("/")
 async def read_root(request:Request):
@@ -63,7 +75,7 @@ async def save_item(cont: str=Form(...)):
         f.write(cont)
     return HTMLResponse(f'''<div hx-post="/edit" hx-swap="outerHTML" class="mockup-code strategy"><pre>
                         <code class="language-python">{cont}</code></pre></div>''')
-
+'''
 @app.post("/run")
 async def run_strategy(symbol: str = Form(...)):
     print(app.state.it)
@@ -75,10 +87,47 @@ async def run_strategy(symbol: str = Form(...)):
     stats_df = pd.DataFrame([results])
     stats_html = stats_df.to_html(justify='left',index=False)    
     return HTMLResponse(stats_html.replace("\n", ""))
+'''
+
+@app.post("/run")
+async def run_strategy(symbol: str = Form(...)):
+    strategy_name = app.state.it
+    #df1 = df.filter(pl.col('symbol') == symbol).drop('symbol').sort('date',descending=False)
+    #df1=df1.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    #df1=app.state.df.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    df1=app.state.df.clone()
+    my_module = SourceFileLoader(strategy_name, f"strategies/{app.state.it}").load_module()
+    stats = my_module.main(df1.to_pandas().set_index('date'))
+    # Format results for display
+    app.state.trades=stats['_trades']
+    stats_df = pd.DataFrame([stats])
+    app.state.stats=stats_df
+    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False)
+    return HTMLResponse(stats_html.replace("\n", ""))
 
 @app.post("/fig")
 async def fig():
-    return HTMLResponse(app.state.fig)
+    #return HTMLResponse(app.state.fig)
+    df=app.state.df.to_pandas()
+    bar = (
+    Candlestick(init_opts=opts.InitOpts(height="600px",width="1320px"))
+    .add_xaxis(df['date'].astype('str').tolist())
+    .add_yaxis(series_name="", y_axis=df[['Open','Close','Low','High']].to_numpy().tolist(),itemstyle_opts=opts.ItemStyleOpts(color="#00da3c",border_color="#00da3c", color0="#ec0000",border_color0="#ec0000"),)
+    #.add_dataset(data=list(df['open','high','low','close','gain'].to_numpy().tolist()),)
+    .set_global_opts(title_opts=opts.TitleOpts(title=app.state.sym), datazoom_opts=[
+        opts.DataZoomOpts(is_show=False,type_="inside",xaxis_index=[0, 1],range_start=90,range_end=100,),
+        opts.DataZoomOpts(is_show=True,xaxis_index=[0, 5],type_="slider",pos_bottom="5%",range_start=85,range_end=100,)],
+        tooltip_opts=opts.TooltipOpts(trigger="axis",axis_pointer_type="cross",background_color="rgba(245, 245, 245, 0.8)",border_width=1,border_color="#ccc",textstyle_opts=opts.TextStyleOpts(color="#000"),),))
+    #print(bar.render_embed())
+#if app.state.tradeplot:
+    s1=Scatter().add_xaxis(app.state.trades['EntryTime'].astype('str').tolist()).add_yaxis('Entry',app.state.trades['EntryPrice'].tolist(),symbol_size=15,symbol='triangle')
+    s2=Scatter().add_xaxis(app.state.trades['ExitTime'].astype('str').tolist()).add_yaxis('Exit',app.state.trades['ExitPrice'].tolist(),symbol_size=15,symbol='triangle')
+    s2.options['series'][0]['symbolRotate']=180
+    s1.options['series'][0]['itemStyle']={ 'color': 'yellow'}
+    s2.options['series'][0]['itemStyle']={ 'color': 'blue'}
+    bar=bar.overlap(s1)
+    bar=bar.overlap(s2)
+    return HTMLResponse(bar.render_embed())
 
 @app.post("/new")
 async def new_item(filename: str=Form(...)):
@@ -92,9 +141,12 @@ async def new_item(filename: str=Form(...)):
 @app.post("/loaddata")
 async def edit_item(symbol: str=Form(...)):
     df1=df.filter(pl.col('symbol')==symbol)
+    df1=df1.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    df1=df1.sort('date',descending=False)
     app.state.df=df1
+    app.state.sym=symbol
     return symbol
-
+'''
 @app.post("/optimize")
 async def run_strategy(symbol: str = Form(...)):
     print(app.state.it)
@@ -110,6 +162,23 @@ async def run_strategy(symbol: str = Form(...)):
     # Format results for display
     stats_df = pd.DataFrame([results])
     stats_html = stats_df.to_html(justify='left',index=False)
+    return HTMLResponse(stats_html.replace("\n", ""))
+'''
+@app.post("/optimize")
+async def run_strategy(symbol: str = Form(...)):
+    strategy_name = app.state.it
+    #df1 = df.filter(pl.col('symbol') == symbol).drop('symbol').sort('date',descending=False)
+    #df1=df1.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    #df1=app.state.df.rename({'open':"Open",'high':'High','low':'Low','close':"Close",'volume':'Volume'})
+    df1=app.state.df.clone()
+    my_module = SourceFileLoader(strategy_name, f"strategies/{app.state.it}").load_module()
+    stats = my_module.optimize(df1.to_pandas().set_index('date'))
+    # Format results for display
+    app.state.trades=stats['_trades']
+    stats_df = pd.DataFrame([stats])
+    app.state.stats=stats_df.copy()
+    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False)
+    app.state.tradeplot=True
     return HTMLResponse(stats_html.replace("\n", ""))
 
 @app.get('/strategy')
--- strategies/macd.py
+++ strategies/macd.py
@@ -1,7 +1,16 @@
 import polars as pl
 import talib as ta
 from typing import List
+import numpy as np
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover, cross
+from pyecharts.charts import Bar, Candlestick, Kline
+from pyecharts import options as opts
+import plotly.io as pio
+#from pyecharts.globals import CurrentConfig, NotebookType
 
+pio.templates.default = "plotly_white"
+'''
 def main(data: pl.DataFrame, short_window: int = 30, long_window: int = 100) -> List[int]:
     """
     Example strategy using moving average crossover
@@ -29,3 +38,54 @@ def main(data: pl.DataFrame, short_window: int = 30, long_window: int = 100) -> 
               macd[i-1] > sig[i-1]):
             signals[i+1] = -1  # Sell signal
     return signals
+'''
+def ta_MACD(df,fastperiod,slowperiod,signalperiod):
+    ""
+    return ta.MACD(df,fastperiod,slowperiod,signalperiod)
+class MACD(Strategy):
+    fastperiod = 10 
+    slowperiod = 150
+    signalperiod = 6
+    def init(self):
+        print(self.data['Close'])
+        price = self.data['Close']
+        self.macd, self.sig, self.hist =self.I(ta_MACD, np.array([float(i) for i in self.data['Close']]), self.fastperiod, self.slowperiod, self.signalperiod)
+        #dd=pd.DataFrame({'macd':self.macd,'sig':self.sig,'hist':self.hist})
+    def next(self):
+        if cross(self.macd, self.sig):
+            if self.position:
+                self.position.close()
+            else:
+                self.buy(size=.9999)
+        elif cross(self.sig, self.macd):
+            if self.position:
+                self.position.close()
+            else:
+                self.sell()
+
+def main(df):
+    bt = Backtest(df, MACD, cash= 100000,exclusive_orders=True,trade_on_close=False,)
+    stats = bt.run()
+    print(stats)
+    print(stats._trades)
+    return stats
+
+def optimize(df):
+    bt = Backtest(df, MACD, cash= 100000,exclusive_orders=True,trade_on_close=False,)
+    stats = bt.optimize(fastperiod=[10,20,30,40,50],slowperiod=range(60,200,5), maximize='Return [%]')
+    #t1=go.Bar(x=['Buy', 'Sell'],y=[len(win_trades), len(loss_trades)], name='Trade Types')
+    # Plot 3: Monthly Returns
+    #returns = pl.Series(self.portfolio_values).pct_change()
+    #monthly_returns = pl.Series(self.portfolio_values).pct_change().to_list()
+    #fig.add_trace(go.Box(y=monthly_returns,name='Monthly Returns'), row=2, col=1)
+    #t2=go.Box(y=monthly_returns,name='Monthly Returns')
+    # Plot 4: Cumulative Returns
+    #cumulative_returns = np.cumprod(1 + pl.Series(self.portfolio_values).pct_change().to_list()) - 1
+    #returns_array = np.array([x for x in returns.to_list() if x is not None])  # Remove None values
+    #cumulative_returns = np.cumprod(1 + returns_array) - 1
+    #dates = self.data['date'].to_list()[1:] 
+    #fig.add_trace(go.Scatter(x=self.data['date'].to_list(),y=cumulative_returns,name='Cumulative Returns',fill='tozeroy'),row=3, col=1)
+    #t3=go.Scatter(x=self.data['date'].to_list(),y=cumulative_returns,name='Cumulative Returns',fill='tozeroy')
+    # Update layout
+    #fig.update_layout(height=800,title_text="Detailed Trade Analysis",showlegend=False)
+    return stats
