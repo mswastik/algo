2c1e8f9b7b66551329654cd251ba4a94f6dfd078
Swastik <m.swastik@gmail.com>
2025-01-03 19:33:51 +0530

    updated layout and charts

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ import polars as pl
 import pandas as pd
 from pyecharts.charts import Bar, Candlestick, Scatter, Line, Grid
 from pyecharts import options as opts
-#import pickle
 from bs4 import BeautifulSoup
 from pyecharts.commons.utils import JsCode
 from data import fyers_login, update_parquet_data
-#import sys
-#sys.path.append("strategies")
-#from macd import MACDStrategy
+import requests
 
 pd.options.display.float_format = '{:,.2f}'.format
 
@@ -51,15 +48,24 @@ fl=fl[1:-1].strip().replace("'","").replace(" ","").split(',')
 #print(fl)
 @app.get("/")
 async def read_root(request:Request):
+    global runs
     seq=os.listdir('strategies')
-    file_list=[f for f in seq if os.path.isfile(os.path.join('strategies',f))] 
-    rd=runs[['Symbol','Strategy','Return (Ann.) [%]']].to_html(justify='left',classes=['table table-sm'])
+    file_list=[f for f in seq if os.path.isfile(os.path.join('strategies',f))]
+    runs=runs.sort_values('Return (Ann.) [%]',ascending=False).reset_index(drop=True)
+    rd=runs[['Symbol','Strategy','Return (Ann.) [%]','Win Rate [%]']].to_html(justify='left',classes=['table table-xs'])
     soup = BeautifulSoup(rd,"html.parser")
     for tag in soup.findAll('tr'):
         if tag.find('th').contents:
+            nt=soup.new_tag("td",attrs={"id":"delete"})
+            nt1=soup.new_tag("button", attrs={"hx-delete":f"/deleteruns/{str(tag.find('th').contents[0])}",
+                            "hx-confirm": "Delete Run?",
+                            "class":"btn btn-sm",})
+            nt1.string="Delete"
             tag['hx-get']='/getruns/'+str(tag.find('th').contents[0])
             tag['hx-target']="#stats"
             tag['class']='cursor-pointer border-b transition-colors hover'
+            tag.insert(-1,nt)
+            tag.find(id="delete").insert(0,nt1)
     return templates.TemplateResponse(request=request,name="index.html",context={"seq":file_list,"fl":fl,'runs':soup})
 
 @app.post("/strategies/{item}")
@@ -105,55 +111,52 @@ async def run_strategy(symbol: str = Form(...)):
     app.state.trades=stats['_trades']
     app.state.equity=stats['_equity_curve']
     stats_df = pd.DataFrame([stats])
-    app.state.stats=stats_df
-    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False)
-    return HTMLResponse(stats_html.replace("\n", ""))
+    app.state.stats=stats_df.copy()
+    stat=stats_df[['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration']]
+    stats_df=stats_df.drop(columns=['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration'])
+    st_html=stat.T.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    #return HTMLResponse(stats_html.replace("\n", ""))
+    return HTMLResponse(f'''<div class="card px-5">{st_html.replace("\n","")}</div>
+                        <div class="card px-5">{stats_html.replace("\n","")}</div>''')
 
 @app.post("/fig")
 async def fig():
     df2=app.state.df.to_pandas().copy()
     df2=df2.round({'Open':1,'Close':1,'Low':1,'High':1})
-    bar = (
+    candle = (
     Candlestick(init_opts=opts.InitOpts(height="600px",width="1320px"))
     .add_xaxis(df2['date'].astype('str').tolist())
-    .add_yaxis(series_name="", y_axis=df2[['Open','Close','Low','High']].to_numpy().tolist(),itemstyle_opts=opts.ItemStyleOpts(color="#00da3c",border_color="#00da3c",
+    .add_yaxis(series_name="Price", y_axis=df2[['Open','Close','Low','High']].to_numpy().tolist(),itemstyle_opts=opts.ItemStyleOpts(color="#00da3c",border_color="#00da3c",
          color0="#ec0000",border_color0="#ec0000"),yaxis_index=0,xaxis_index=0)
     .set_global_opts(title_opts=opts.TitleOpts(title=app.state.sym,pos_left="7%",pos_top="1%"),
         legend_opts=opts.LegendOpts(pos_top="2%",orient='horizontal'),
-        # datazoom_opts=[opts.DataZoomOpts(type_="inside"),opts.DataZoomOpts(pos_top="middle",xaxis_index=0,range_end="100%")],
-        tooltip_opts=opts.TooltipOpts(trigger="axis",axis_pointer_type="cross",background_color="rgba(245, 245, 245, 0.8)",border_width=1,border_color="#ccc",textstyle_opts=opts.TextStyleOpts(color="#000"),),))
-    s1=Scatter().add_xaxis(app.state.trades['EntryTime'].astype('str').tolist()).add_yaxis('Entry',app.state.trades['EntryPrice'].tolist(),symbol_size=15,symbol='triangle',
+        tooltip_opts=opts.TooltipOpts(trigger="axis",axis_pointer_type="cross",background_color="rgba(245, 245, 245, 0.8)",border_width=1,border_color="#ccc"
+        ,textstyle_opts=opts.TextStyleOpts(color="#000"),),
+         yaxis_opts=opts.AxisOpts(is_scale=True,)))
+    s1=Scatter().add_xaxis(app.state.trades['EntryTime'].astype('str').tolist()).add_yaxis('Entry',app.state.trades['EntryPrice'].tolist(),symbol_size=15,symbol='triangle',yaxis_index=0,
     label_opts=opts.LabelOpts(is_show=False))
-    s2=Scatter().add_xaxis(app.state.trades['ExitTime'].astype('str').tolist()).add_yaxis('Exit',app.state.trades['ExitPrice'].tolist(),symbol_size=15,symbol='triangle',
-        #label_opts=opts.LabelOpts(is_show=True,formatter=JsCode("function(param) {return Number(param.data[1]).toFixed(0);}")))
+    s2=Scatter().add_xaxis(app.state.trades['ExitTime'].astype('str').tolist()).add_yaxis('Exit',app.state.trades['ExitPrice'].tolist(),symbol_size=15,symbol='triangle',yaxis_index=0,
         label_opts=opts.LabelOpts(is_show=False))
     s2.options['series'][0]['symbolRotate']=180
     s1.options['series'][0]['itemStyle']={ 'color': 'brown'}
     s2.options['series'][0]['itemStyle']={ 'color': 'blue'}
-    bar=bar.overlap(s1)
-    bar=bar.overlap(s2)
+    candle=candle.overlap(s1)
+    candle=candle.overlap(s2)
     for i,n in app.state.trades.iterrows():
         col='red' if n['PnL']<=0 else 'green'
         l=Line().add_xaxis([n['EntryTime'].strftime("%Y-%m-%d"),n['ExitTime'].strftime("%Y-%m-%d")]).add_yaxis(series_name='Trades',y_axis=[n['EntryPrice'],n['ExitPrice']],label_opts=opts.LabelOpts(is_show=False)
-        ,linestyle_opts=opts.LineStyleOpts(width=2,color=col))
-        bar.overlap(l)
-    #bar.extend_axis(yaxis=opts.AxisOpts(position="right",min_=0,max_=100,)).add_yaxis('Returns',app.state.equity['DrawdownPct'].tolist(),yaxis_index=1)
-    print(df2)
-    print(app.state.equity.reset_index()[['date','DrawdownPct']])
-    df2=df2.join(app.state.equity['DrawdownPct'],on='date',how='left')
-    area = Line().add_xaxis(df2['date'].astype('str').tolist()).add_yaxis('Returns',df2['DrawdownPct'].round(3).tolist(),
-                                    areastyle_opts=opts.AreaStyleOpts(opacity=.6),xaxis_index=0,yaxis_index=1).set_global_opts(
-        yaxis_opts=opts.AxisOpts(position='right'))
-    #bar.overlap(area, yaxis_index=1, is_add_yaxis=True)
+        ,linestyle_opts=opts.LineStyleOpts(width=2,color=col),yaxis_index=0,xaxis_index=0)
+        candle.overlap(l)
+    area = Line().add_xaxis(df2['date'].astype('str').tolist()).add_yaxis('Volume',df2['Volume'].tolist(),
+                                    areastyle_opts=opts.AreaStyleOpts(opacity=.6),label_opts=opts.LabelOpts(is_show=False),xaxis_index=0,yaxis_index=1).set_global_opts(yaxis_opts=opts.AxisOpts(
+                        position='right'),xaxis_opts=opts.AxisOpts(is_show=False),legend_opts=opts.LegendOpts(pos_left="53%"))
     trc=app.state.trades.copy()
-    #trp=trc.copy() #for cumulative PnL for Portfolio value
-    #trp['CumProfit']=trp['PnL'].cumsum()
     trc=trc.resample('ME',on='ExitTime').sum(numeric_only=True)
     trc['CumProfit']=trc['PnL'].cumsum()
     cuma = Line(init_opts=opts.InitOpts(height="150px",width="820px")).add_xaxis(trc.index.astype('str').tolist()).add_yaxis('Returns',trc['CumProfit'].tolist(),
-            areastyle_opts=opts.AreaStyleOpts(opacity=.6),yaxis_index=3,xaxis_index=1,label_opts=opts.LabelOpts(is_show=False)).set_global_opts(
-        yaxis_opts=opts.AxisOpts(position='right'))
-    #print(trc)
+            areastyle_opts=opts.AreaStyleOpts(opacity=.6),yaxis_index=2,xaxis_index=2,label_opts=opts.LabelOpts(is_show=False)).set_global_opts(
+        yaxis_opts=opts.AxisOpts(position='right'),legend_opts=opts.LegendOpts(pos_left="40%"))
     color_function = """
         function (params) {
             if (params.value > 0) {
@@ -163,25 +166,19 @@ async def fig():
             }}
         """
     bar1=(Bar(init_opts=opts.InitOpts(height="300px",width="820px")).add_xaxis(trc.index.astype('str').tolist()).add_yaxis("PnL",trc['PnL'].round(0).tolist(),
-        #label_opts=opts.LabelOpts(is_show=True,formatter=JsCode("function(param) {return Number(param.data[1]).toFixed(0);}")))
-        label_opts=opts.LabelOpts(is_show=True,color='black',position='outside',vertical_align='top'),
-        itemstyle_opts=opts.ItemStyleOpts(color=JsCode(color_function)),yaxis_index=2,xaxis_index=1)
-        #.extend_axis(yaxis=opts.AxisOpts(position="right",min_=0,max_=100,axisline_opts=opts.AxisLineOpts(
-        #        linestyle_opts=opts.LineStyleOpts(color="#d14a61")),))
-        #.add_yaxis('Returns',trc['CumProfit'].tolist(),yaxis_index=1)
-        )
-    #bar1.set_global_opts(datazoom_opts=opts.DataZoomOpts(is_show=False,is_disabled=True))
-    bar1.set_series_opts(label_opts=opts.LabelOpts(position="end"))
-    #bar1.overlap(cuma)
-    #line=Line().add_xaxis(trc.index.astype('str').tolist()).add_yaxis("Return",trc['ReturnPct'].tolist())
-    grid = (Grid(init_opts=opts.InitOpts(height="1000px",width="1320px")).add(bar, grid_opts=opts.GridOpts(pos_bottom="55%"),grid_index=0)
-            .add(bar1, grid_opts=opts.GridOpts(pos_bottom="3%",pos_top="58%"),grid_index=1)
-            .add(area, grid_opts=opts.GridOpts(pos_bottom="55%"))
-            .add(cuma, grid_opts=opts.GridOpts(pos_bottom="3%",pos_top="58%")))
-    grid.options['dataZoom'] = [opts.DataZoomOpts(type_="inside",yaxis_index=0,range_end="100%"),opts.DataZoomOpts(xaxis_index=0,pos_top="48%",range_end="100%",is_zoom_on_mouse_wheel= True, type_= "slider")]
+        label_opts=opts.LabelOpts(is_show=True,color='black',position='outside'),
+        itemstyle_opts=opts.ItemStyleOpts(color=JsCode(color_function)),yaxis_index=2,xaxis_index=2))
+    
+    grid = (Grid(init_opts=opts.InitOpts(height="1000px",width="1320px")).add(candle, grid_opts=opts.GridOpts(pos_bottom="45%"),grid_index=0)
+            .add(area, grid_opts=opts.GridOpts(pos_bottom="45%"),grid_index=1)
+            .add(bar1, grid_opts=opts.GridOpts(pos_bottom="3%",pos_top="68%"),grid_index=2)
+            .add(cuma, grid_opts=opts.GridOpts(pos_bottom="3%",pos_top="68%"),grid_index=3))
+    grid.options['dataZoom'] = [opts.DataZoomOpts(xaxis_index=[0,1],pos_bottom="39%",range_start="80",range_end="100"),
+                                opts.DataZoomOpts(type_="inside",xaxis_index=[0,1],),
+                                opts.DataZoomOpts(type_="inside",yaxis_index=0,range_end="100")]
     #grid.options['Legend'] = opts.LegendOpts(orient='horizontal',pos_top="2%")
     htstr=grid.render_embed()
-    soup=BeautifulSoup(htstr,features="lxml")
+    soup=BeautifulSoup(htstr,features="html.parser")
     print(app.state.equity)
     return HTMLResponse(soup.body)
 
@@ -211,10 +208,15 @@ async def run_strategy(symbol: str = Form(...)):
     app.state.equity=stats[0]['_equity_curve']
     stats_df = pd.DataFrame([stats[0]])
     stats_df['best_params']=[stats[1].x]
-    print(stats_df)
+    #print(stats_df)
     app.state.stats=stats_df.copy()
-    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False)
-    return HTMLResponse(stats_html.replace("\n", ""))
+    stat=stats_df[['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration']]
+    stats_df=stats_df.drop(columns=['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration'])
+    st_html=stat.T.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    stats_html = stats_df.iloc[:,0:-3].T.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    #return HTMLResponse(stats_html.replace("\n", ""))
+    return HTMLResponse(f'''<div class="card px-5">{st_html.replace("\n","")}</div>
+                        <div class="card px-5">{stats_html.replace("\n","")}</div>''')
 
 @app.get('/strategy')
 async def page(request:Request):
@@ -235,13 +237,19 @@ async def page(request:Request):
 @app.get('/getruns/{ind}')
 async def page(request:Request,ind:int):
     rd=pd.DataFrame(runs.iloc[ind])
+    print(len(rd.loc['_trades']))
     app.state.trades=pd.DataFrame(rd.loc['_trades'][ind])
     app.state.equity=pd.DataFrame(rd.loc['_equity_curve'][ind])
     rd=rd.drop(['_equity_curve','_trades'])
     rd=rd.rename(columns={0:"Stats"})
     app.state.sym=rd.loc['Symbol'].values[0]
     app.state.df=df.filter(pl.col('symbol')==app.state.sym)
-    return HTMLResponse(rd.to_html(justify='left',classes=['table table-xs']))
+    stat=rd.loc[rd.index.isin(['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration'])]
+    stats_df=rd.drop(index=['Return (Ann.) [%]','Win Rate [%]','Sharpe Ratio','Max. Drawdown [%]','Max. Drawdown Duration'])
+    st_html=stat.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    stats_html = stats_df.to_html(justify='left',index=True,header=False,classes=['table table-xs'])
+    return HTMLResponse(f'''<div class="card px-5">{st_html.replace("\n","")}</div>
+                        <div class="card px-5">{stats_html.replace("\n","")}</div>''')
 
 @app.get('/data')
 async def page(request:Request):
@@ -261,7 +269,17 @@ async def getd(sym: str=Form()):
 async def getd():
     app.state.fyers=fyers_login()
 
-@app.get('/reload')
-async def reload():
-    global df
-    df=load()
+
+@app.delete('/deleteruns/{ind}')
+async def delruns(request:Request,ind:int):
+    global runs
+    print(runs)
+    print(runs[runs.index!=ind])
+    runs=runs[runs.index!=ind]
+    runs.to_pickle('runs.pkl')
+
+@app.get('/results')
+async def results(request:Request):
+    url=f"https://www.nseindia.com/api/results-comparision?symbol={app.state.sym}"
+    r=requests.get(url)
+    print(r)
